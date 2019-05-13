e3058b94794902577f7d85027148009098691b1d
dabaiwan <774999134@qq.com>
2019-05-13 23:58:32 +0800

    x.say_name()

--- Car.py
+++ Car.py
@@ -1,11 +1,15 @@
 class Car:
-    def __init__(self, speed=0):
+    def __init__(self, speed=0,name=""):
         self.speed = speed
         self.odometer = 0
         self.time = 0
+        self.name = name
 
-    def say_state(self):
+    def say_speed(self):
         self.print_state()
 
     def print_state(self):
         print("I'm going {} kph".format(self.speed))
+
+    def say_name(self):
+        print("{}".format(self.name))
--- cartest.py
+++ cartest.py
@@ -1,7 +1,9 @@
 from Car import Car
 
-x = Car(40)
+x = Car(40,"lufengx2")
 
-x.say_state()
+x.say_speed()
 
 x.print_state()
+
+x.say_name()
